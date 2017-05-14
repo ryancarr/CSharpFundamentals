bc17d7a3e40a9cacf11409604d28e004c4bcb9c1
ryancarr <rycarr@gmail.com>
2017-05-14 18:55:05 -0500

    Commit Exercise Chapter 5 #1

---
+++ Basics/Exercise5.1/Exercise5.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5._1
+{
+    class Program
+    {
+        static string ValidateNumber(int input, int min, int max)
+        {
+            string result;
+            if (input <= max && input >= min)
+                result = "Valid";
+            else
+                result = "Invalid";
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            const int min = 1;
+            const int max = 10;
+            int input;
+
+            Console.Write("Please enter a number between {0} and {1}: ", min, max);
+            input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(ValidateNumber(input, min, max));
+        }
+    }
+}
