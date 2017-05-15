89dc16b3d6f5b3a65c5dea712c8397401d195b3f
ryancarr <rycarr@gmail.com>
2017-05-14 19:08:24 -0500

    Added Exercise Chapter 5 # 2

---
+++ Basics/Exercise5.2/Exercise5.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise5._2
+{
+    // Task: Write a program which takes two numbers from the console and displays the maximum of the two.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+
+            Console.Write("Please enter a number: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter another number: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (secondNumber > firstNumber)
+                Console.WriteLine("{0} is greater than {1}", secondNumber, firstNumber);
+            else
+                Console.WriteLine("{0} is greater than {1}", firstNumber, secondNumber);
+        }
+    }
+}
