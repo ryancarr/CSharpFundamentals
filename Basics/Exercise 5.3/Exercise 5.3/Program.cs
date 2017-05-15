48b79ff0987437169f37084f7f747d039b5b0e6c
ryancarr <rycarr@gmail.com>
2017-05-14 19:24:44 -0500

    Created Exercise Chapter 5 #3

---
+++ Basics/Exercise 5.3/Exercise 5.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_5._3
+{
+    // Task: Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width;
+            int height;
+            string orientation;
+
+            Console.Write("Please enter the width of the picture: ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter the height of the picture: ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            if (height > width)
+                orientation = "The picture is in portrait orientation.";
+            else
+                orientation = "The picture is in landscape orientation.";
+
+            Console.WriteLine(orientation);
+        }
+    }
+}
