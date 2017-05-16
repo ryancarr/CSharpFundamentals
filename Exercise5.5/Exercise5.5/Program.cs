dab17b3fa4b374dda1423faeaf658df8251731c4
Ryan Carr <rycarr@gmail.com>
2017-05-16 10:57:06 -0500

    Created Exercise Chapter 5  #5

---
+++ Exercise5.5/Exercise5.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise5._5
+{
+    // Task: Write a program to count how many numbers between 1 and 100 are evenly divisible by 3. Display the count on the console
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    count += 1;
+            }
+
+            Console.WriteLine("There are {0} numbers evenly divided by 3 between 1 and 100.", count);
+        }
+    }
+}
