791e02a4e6e9a7a07148f841376131a599ff3970
Ryan Carr <rycarr@gmail.com>
2017-05-18 14:52:49 -0500

    Removed duplicates Created Exercise Chapter 5 #7

---
+++ Basics/Exercise5.7/Exercise5.7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5._7
+{
+    // Task: Write a program that asks a user for a number then computes the factorial for the number.
+    //       Display output to console in format 5! = 120
+    class Program
+    {
+        static int Factorial(int number)
+        {
+            int result;
+
+            if (number == 0)
+                result = 1;
+            else
+                result = Factorial(number - 1) * number;
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string input;
+            int factorial;
+
+            Console.WriteLine("Enter a number: ");
+            input = Console.ReadLine();
+
+            factorial = Factorial(Convert.ToInt32(input));
+            Console.WriteLine("{0}! = {1}", input, factorial);
+        }
+    }
+}
--- Basics/Exercise5.7/Exercise5.7/Properties/AssemblyInfo.cs
+++ Basics/Exercise5.7/Exercise5.7/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Exercise5.6")]
+[assembly: AssemblyTitle("Exercise5.7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Exercise5.6")]
+[assembly: AssemblyProduct("Exercise5.7")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a5e0af23-0ecb-41fe-9974-0c4ea8974855")]
+[assembly: Guid("84af2a02-f8ff-48be-b35a-8b4c810c7549")]
 
 // Version information for an assembly consists of the following four values:
 //
