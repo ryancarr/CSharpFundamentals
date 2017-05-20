ce7ae811fbe10f44b6263b339bcab2040eb9a4df
Ryan Carr <rycarr@gmail.com>
2017-05-20 12:31:05 -0500

    Created Exercise Chapter 5 #9

---
+++ Basics/Exercise5.9/Exercise5.9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise5._9
+{
+    // Tasks: Write a program that asks a user to enter a string of numbers separated by commas. Then display the maximum number to the console.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int max = -10*10000;
+
+            Console.WriteLine("Please write a series of numbers separated by commas");
+            input = Console.ReadLine();
+
+            var convertedInput = input.Split(',');
+
+            foreach (var number in convertedInput)
+            {
+                var temp = Convert.ToInt32(number);
+                if (temp > max)
+                    max = temp;
+                else
+                    continue;
+            }
+
+            Console.WriteLine("The max number is: {0}",max);
+        }
+    }
+}
