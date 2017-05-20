2baf40a659c9e2b4a4d96d50ba4aafe660103259
Ryan Carr <rycarr@gmail.com>
2017-05-20 16:28:28 -0500

    Created Exercise Chapter 6 #1

---
+++ Basics/Exercise6.1/Exercise6.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise6._1
+{
+    // Tasks: Create a program that continuously asks the user for names until they are done entering. Output format = [Friend 1], [Friend 2], and 10 others.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+            string input;
+            string output = "";
+            int count = 0;
+
+            do
+            {
+                Console.Write("Please enter a name, type ok if you are done: ");
+                input = Console.ReadLine();
+
+                if (input.ToLower() != "ok")
+                    names.Add(input);
+
+            } while (input.ToLower() != "ok");
+
+            count = names.Count;
+
+            if (count >= 3)
+            {
+                output = String.Format("{0}, {1} and {2} others liked your status.", names[0], names[1], count - 2);
+            }
+            else if (count == 2)
+            {
+                output = String.Format("{0} and {1} liked your status.", names[0], names[1]);
+            }
+            else if (count == 1)
+            {
+                output = String.Format("{0} liked your status.", names[0]);
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
